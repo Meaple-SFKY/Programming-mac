6130ce6e7e4f925037a8385d6489199e589fd713
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-03-24 12:10:28 +0800

    java

---
+++ Java_Folder/src/practice/mathexpe/Interaction.java
@@ -0,0 +1,13 @@
+package src.practice.mathexpe;
+
+public class Interaction {
+    public static void main(String[] args) {
+        int k = 0;
+        double temp = Math.PI / 4;
+        while (k < 20) {
+            
+            System.out.println(temp);
+            k += 1;
+        }    
+    }
+}
