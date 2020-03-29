2e385cbc7e986bb1e4fc5a305563369baa2974ea
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-03-29 13:42:14 +0800

    java

--- Java_Folder/src/temporary/magic/Demo.java
+++ Java_Folder/src/temporary/magic/Demo.java
@@ -2,57 +2,100 @@ package src.temporary.magic;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Demo {
+public class Demo extends JFrame implements MouseListener {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame();
-        JMenuBar jMenuBar = new JMenuBar();
-        jFrame.setJMenuBar(jMenuBar);
-        JMenu jMenuOne = new JMenu("开始");
-        JMenu jMenuTwo = new JMenu("文件");
-        JMenu jMenuThree = new JMenu("编辑");
-        JMenu jMenuFour = new JMenu("选择");
-        JMenu jMenuFive = new JMenu("查看");
-        jMenuBar.add(jMenuOne);
-        jMenuBar.add(jMenuTwo);
-        jMenuBar.add(jMenuThree);
-        jMenuBar.add(jMenuFour);
-        jMenuBar.add(jMenuFive);
-        JMenuItem jMenuItem1 = new JMenuItem("关于");
-        JMenuItem jMenuItem2 = new JMenuItem("个性化");
-        JMenuItem jMenuItem3 = new JMenuItem("外观");
-        JMenuItem jMenuItem4 = new JMenuItem("新建文件");
-        JMenuItem jMenuItem5 = new JMenuItem("打开文件");
-        JMenuItem jMenuItem6 = new JMenuItem("打开文件夹");
-        JMenuItem jMenuItem7 = new JMenuItem("剪切");
-        JMenuItem jMenuItem8 = new JMenuItem("复制");
-        JMenuItem jMenuItem9 = new JMenuItem("粘贴");
-        JMenuItem jMenuItem10 = new JMenuItem("全选");
-        JMenuItem jMenuItem11 = new JMenuItem("反选");
-        JMenuItem jMenuItem12 = new JMenuItem("取消选择");
-        JMenuItem jMenuItem13 = new JMenuItem("错误");
-        JMenuItem jMenuItem14 = new JMenuItem("终端");
-        JMenuItem jMenuItem15 = new JMenuItem("输出");
-        jMenuOne.add(jMenuItem1);
-        jMenuOne.add(jMenuItem2);
-        jMenuOne.add(jMenuItem3);
-        jMenuTwo.add(jMenuItem4);
-        jMenuTwo.add(jMenuItem5);
-        jMenuTwo.add(jMenuItem6);
-        jMenuThree.add(jMenuItem7);
-        jMenuThree.add(jMenuItem8);
-        jMenuThree.add(jMenuItem9);
-        jMenuFour.add(jMenuItem10);
-        jMenuFour.add(jMenuItem11);
-        jMenuFour.add(jMenuItem12);
-        jMenuFive.add(jMenuItem13);
-        jMenuFive.add(jMenuItem14);
-        jMenuFive.add(jMenuItem15);
-        jFrame.setBounds(300, 300, 500, 400);
-        JTextField jTextField = new JTextField(10);
-        jTextField.setText("Hello");
-        jFrame.add(jTextField);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setVisible(true);
+        Demo demo = new Demo();
     }
+
+    public int x, y, sign, width, height;
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int X) {
+        this.x = X;
+    }
+
+    public void setY(int Y) {
+        this.y = Y;
+    }
+
+    public void setFlag(int flag) {
+        sign = flag;
+    }
+
+    public int getFlag() {
+        return this.sign;
+    }
+
+    public Demo() {
+        addMouseListener(this);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        width = (int)screenSize.getWidth() / 3;
+        height = (int)screenSize.getHeight() / 3;
+        setBounds(width, height, width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+        int flag = getFlag();
+        if (flag == 1) {
+            g.setColor(Color.GREEN);
+            g.drawOval(x, y, 20, 20);
+        }
+        else if (flag == 2) {
+            g.setColor(Color.RED);
+            g.drawRect(x, y, 20, 20);
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        setX(e.getX());
+        setY(e.getY());
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            setFlag(1);
+            repaint();
+        }
+        else if (e.getButton() == MouseEvent.BUTTON3) {
+            setFlag(2);
+            repaint();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+    
 }
