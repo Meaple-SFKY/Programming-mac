c4fc34be3684feb3a63db7dcc8cef0a11c79d347
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-04-26 14:17:20 +0800

    cpp

--- C_Folder/Maze/main.cpp
+++ C_Folder/Maze/main.cpp
@@ -1,11 +1,18 @@
-#include"maze.hpp"
+#include "maze.hpp"
 
 int main(void) {
     srand((unsigned int)time(NULL));
     int size;
+    Point entrance, destination;
     cout << "Please enter the size: ";
     cin >> size;
+    cout << "Please enter the X-axis and the Y-axis of your entrance: ";
+    cin >> entrance.X_Locat >> entrance.Y_Locat;
+    cout << "Please enter the X-axis and the Y-axis of your destination: ";
+    cin >> destination.X_Locat >> destination.Y_Locat;
     Maze maze(size);
+    maze.setStartPoint(entrance);
+    maze.setEndPoint(destination);
     maze.printMaze(maze.MAZE);
     return 0;
 }
--- C_Folder/Maze/maze.hpp
+++ C_Folder/Maze/maze.hpp
@@ -1,18 +1,23 @@
 #ifndef _MAZE_HPP_
 #define _MAZE_HPP_
 
-#include<iostream> 
-#include<ctime>
+#include <fstream>
+#include <iostream>
+#include <sstream> 
+#include <ctime>
+#include <string>
 
 using namespace std;
 
 #define SQUARE "██"
 #define SPACE "  "
-#define MYSELF "★"
+#define MYSELF "★★"
 #define DOWNARROW "↓↓"
 #define UPARROW "↑↑"
 #define LEFTARRROW "←←"
 #define RIGHTARROW "→→"
+#define ENTRANCE "◉◉"
+#define DESTINATION "▼▲"
 #define WALLR 2
 #define WALL 1
 #define ROAD 0
@@ -23,37 +28,58 @@ using namespace std;
 #define LEFT 2
 #define RIGHT 3
 
+typedef struct point {
+    int X_Locat;
+    int Y_Locat; 
+} Point;
+
 typedef struct {
     int** array;
     int** read;
-    int Start_X_Locat;
-    int Start_Y_Locat;
-    int End_X_Locat;
-    int End_Y_Locat;
+    Point startPoint;
+    Point endPoint;
     int size;
 } mazeInfo;
 
 class Maze {
 private:
     int getSeed(void);
-    bool ReadJudge(mazeInfo maze, int X_Locat, int Y_Locat);
-    bool Around(mazeInfo maze, int X_Locat, int Y_Locat);
-    void WallAround(void);
-    void EmptyLine(void);
+    string mode;
+
+    bool readJudge(mazeInfo maze, Point pointReadJudge);
+    bool around(mazeInfo maze, Point pointAround);
+    void wallAround(void);
+    void emptyLine(void);
 
 public:
     mazeInfo MAZE;
+
     Maze(int size);
     ~Maze() {};
+
+    void setMode(string modeStr);
+    string getMode(void);
     void printMaze(mazeInfo maze);
-    void Openpath(mazeInfo& maze, int X_Locat, int Y_Locat);
+    void setStartPoint(Point pointStartPoint);
+    void setEndPoint(Point pointStartPoint);
+    void editWallPoint(Point pointSetWall);
+    void editRoadPoint(Point pointSetRoad);
+    void openpath(mazeInfo& maze, Point pointOpenPath);
+    void setInfoCommon(void);
+    void setInfoFromFile(string fileNamePath);
 };
 
 Maze::Maze(int size) {
     MAZE.size = size;
+    Point pointOpenPath;
+
+    pointOpenPath.X_Locat = 1;
+    pointOpenPath.Y_Locat = 1;
+
     if (MAZE.size % 2 == 0) {
         MAZE.size++;
     }
+
     cout << "Now its size is " << MAZE.size << " * " << MAZE.size << "......" << endl;
     int** a = new int* [MAZE.size];
     int** b = new int* [MAZE.size];
@@ -63,6 +89,7 @@ Maze::Maze(int size) {
     }
     MAZE.array = a;
     MAZE.read = b;
+
     for (int i = 0; i < MAZE.size; i++) {
         for (int j = 0; j < MAZE.size; j++) {
             MAZE.array[i][j] = WALL;
@@ -74,19 +101,21 @@ Maze::Maze(int size) {
             MAZE.array[i][j] = WALLR;
         }
     }
-    MAZE.Start_X_Locat = 1;
-    MAZE.Start_X_Locat = 1;
-    MAZE.End_X_Locat = MAZE.size - 2;
-    MAZE.End_X_Locat = MAZE.size - 2;
-    Openpath(MAZE, 1, 1);
+
+    MAZE.startPoint.X_Locat = 1;
+    MAZE.startPoint.Y_Locat = 1;
+    MAZE.endPoint.X_Locat = MAZE.size - 2;
+    MAZE.endPoint.Y_Locat = MAZE.size - 2;
+
+    openpath(MAZE, pointOpenPath);
 }
 
 int Maze::getSeed(void) {
     return rand() % 4;
 }
 
-bool Maze::ReadJudge(mazeInfo maze, int X_Locat, int Y_Locat) {
-    if (maze.read[X_Locat][Y_Locat] == READED) {
+bool Maze::readJudge(mazeInfo maze, Point pointReadJudge) {
+    if (maze.read[pointReadJudge.X_Locat][pointReadJudge.Y_Locat] == READED) {
         return true;
     }
     else {
@@ -94,67 +123,78 @@ bool Maze::ReadJudge(mazeInfo maze, int X_Locat, int Y_Locat) {
     }
 }
 
-bool Maze::Around(mazeInfo maze, int X_Locat, int Y_Locat) {
-    if (((X_Locat > 2) && (maze.array[X_Locat - 2][Y_Locat] == WALLR)) || ((X_Locat < maze.size - 3) && (maze.array[X_Locat + 2][Y_Locat] == WALLR))
-        || ((Y_Locat > 2) && (maze.array[X_Locat][Y_Locat - 2] == WALLR)) || ((Y_Locat < maze.size - 3) && (maze.array[X_Locat][Y_Locat + 2] == WALLR)))
+bool Maze::around(mazeInfo maze, Point pointAround) {
+    if (((pointAround.X_Locat > 2) && (maze.array[pointAround.X_Locat - 2][pointAround.Y_Locat] == WALLR)) || ((pointAround.X_Locat < maze.size - 3) && (maze.array[pointAround.X_Locat + 2][pointAround.Y_Locat] == WALLR))
+        || ((pointAround.Y_Locat > 2) && (maze.array[pointAround.X_Locat][pointAround.Y_Locat - 2] == WALLR)) || ((pointAround.Y_Locat < maze.size - 3) && (maze.array[pointAround.X_Locat][pointAround.Y_Locat + 2] == WALLR)))
     {
         return true;
     }
     return false;
 }
 
-void Maze::Openpath(mazeInfo& maze, int X_Locat, int Y_Locat) {
-    if (X_Locat % 2 == 0) {
-        X_Locat -= 1;
+void Maze::openpath(mazeInfo& maze, Point pointOpenPath) {
+    if (pointOpenPath.X_Locat % 2 == 0) {
+        if (pointOpenPath.X_Locat == 0) {
+            pointOpenPath.X_Locat++;
+        }
+        else {
+            pointOpenPath.X_Locat--;
+        }
     }
-    if (Y_Locat % 2 == 0) {
-        Y_Locat -= 1;
+    if (pointOpenPath.Y_Locat % 2 == 0) {
+        if (pointOpenPath.Y_Locat == 0) {
+            pointOpenPath.Y_Locat++;
+        }
+        else {
+            pointOpenPath.Y_Locat--;
+        }
     }
+
     while (1) {
-        if (Around(maze, X_Locat, Y_Locat) == false) {
+        if (around(maze, pointOpenPath) == false) {
             return;
         }
         else {
-            maze.array[X_Locat][Y_Locat] = ROAD;
+            maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat] = ROAD;
             while (1) {
                 int temp = getSeed();
-                if (temp == UP && X_Locat > 2 && (maze.array[X_Locat - 2][Y_Locat] == WALLR)) {
-                    X_Locat -= 2;
-                    maze.array[X_Locat + 1][Y_Locat] = ROAD;
-                    maze.array[X_Locat][Y_Locat] = ROAD;
+                if (temp == UP && pointOpenPath.X_Locat > 2 && (maze.array[pointOpenPath.X_Locat - 2][pointOpenPath.Y_Locat] == WALLR)) {
+                    pointOpenPath.X_Locat -= 2;
+                    maze.array[pointOpenPath.X_Locat + 1][pointOpenPath.Y_Locat] = ROAD;
+                    maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat] = ROAD;
                 }
-                else if (temp == DOWN && X_Locat < maze.size - 3 && (maze.array[X_Locat + 2][Y_Locat] == WALLR)) {
-                    X_Locat += 2;
-                    maze.array[X_Locat - 1][Y_Locat] = ROAD;
-                    maze.array[X_Locat][Y_Locat] = ROAD;
+                else if (temp == DOWN && pointOpenPath.X_Locat < maze.size - 3 && (maze.array[pointOpenPath.X_Locat + 2][pointOpenPath.Y_Locat] == WALLR)) {
+                    pointOpenPath.X_Locat += 2;
+                    maze.array[pointOpenPath.X_Locat - 1][pointOpenPath.Y_Locat] = ROAD;
+                    maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat] = ROAD;
                 }
-                else if (temp == LEFT && Y_Locat > 2 && (maze.array[X_Locat][Y_Locat - 2] == WALLR)) {
-                    Y_Locat -= 2;
-                    maze.array[X_Locat][Y_Locat + 1] = ROAD;
-                    maze.array[X_Locat][Y_Locat] = ROAD;
+                else if (temp == LEFT && pointOpenPath.Y_Locat > 2 && (maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat - 2] == WALLR)) {
+                    pointOpenPath.Y_Locat -= 2;
+                    maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat + 1] = ROAD;
+                    maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat] = ROAD;
                 }
-                else if (temp == RIGHT && Y_Locat < maze.size - 3 && (maze.array[X_Locat][Y_Locat + 2] == WALLR)) {
-                    Y_Locat += 2;
-                    maze.array[X_Locat][Y_Locat - 1] = ROAD;
-                    maze.array[X_Locat][Y_Locat] = ROAD;
+                else if (temp == RIGHT && pointOpenPath.Y_Locat < maze.size - 3 && (maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat + 2] == WALLR)) {
+                    pointOpenPath.Y_Locat += 2;
+                    maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat - 1] = ROAD;
+                    maze.array[pointOpenPath.X_Locat][pointOpenPath.Y_Locat] = ROAD;
                 }
                 else {
                     break;
                 }
-                Openpath(maze, X_Locat, Y_Locat);
+                openpath(maze, pointOpenPath);
             }
         }
     }
 }
 
-void Maze::WallAround(void) {
+void Maze::wallAround(void) {
     for (int i = 0; i < 30; i++) {
         cout << SQUARE;
     }
     cout << endl;
 }
 
-void Maze::EmptyLine(void) {
+void Maze::emptyLine(void) {
     cout << SQUARE;
     for (int i = 0; i < 28; i++) {
         cout << SPACE;
@@ -162,17 +202,86 @@ void Maze::EmptyLine(void) {
     cout << SQUARE << endl;
 }
 
+void Maze::setMode(string modeStr) {
+    mode = modeStr;
+}
+
+string Maze::getMode(void) {
+    return mode;
+}
+
 void Maze::printMaze(mazeInfo maze) {
+    for (int i = 0; i < maze.size; i++) {
+        if (i < 10) {
+            cout << i << " ";
+        }
+        else {
+            cout << i;
+        }
+    }
+    cout << endl;
+
     for (int x = 0; x < maze.size; x++) {
         for (int y = 0; y < maze.size; y++) {
-            if (maze.array[x][y] == WALL) {
+            if (x == MAZE.startPoint.X_Locat && y == MAZE.startPoint.Y_Locat) {
+                cout << ENTRANCE;
+            }
+            else if (x == MAZE.endPoint.X_Locat && y == MAZE.endPoint.Y_Locat) {
+                cout << DESTINATION;
+            }
+            else if (maze.array[x][y] == WALL) {
                 cout << SQUARE;
             }
             else {
                 cout << SPACE;
             }
         }
-        cout << endl;
+        cout << " " << x << endl;
+    }
+}
+
+void Maze::setStartPoint(Point start) {
+    if (MAZE.array[start.X_Locat][start.Y_Locat] == WALL) {
+        cout << "The entrance is already part of the wall, please reset the entrance..." << endl;
+    }
+    else {
+        MAZE.startPoint.X_Locat = start.X_Locat;
+        MAZE.startPoint.Y_Locat = start.Y_Locat;
+    }
+}
+
+void Maze::setEndPoint(Point end) {
+    if (MAZE.array[end.X_Locat][end.Y_Locat] == WALL) {
+        cout << "The destination is already part of the wall, please reset the destination..." << endl;
+    }
+    else {
+        MAZE.endPoint.X_Locat = end.X_Locat;
+        MAZE.endPoint.Y_Locat = end.Y_Locat;
+    }
+}
+
+void Maze::editWallPoint(Point pointSetWall) {
+    MAZE.array[pointSetWall.X_Locat][pointSetWall.Y_Locat] = WALL;
+}
+
+void Maze::editRoadPoint(Point pointSetRoad) {
+    MAZE.array[pointSetRoad.X_Locat][pointSetRoad.Y_Locat] = ROAD;
+}
+
+void Maze::setInfoFromFile(string fileNamePath) {
+    ifstream infoFile(fileNamePath);
+    string temp;
+    stringstream stringToInteger;
+
+    if (!infoFile.is_open()) {
+        cout << "File failed to open." << endl;
+    }
+
+    while (getline(infoFile, temp)) {
+        for (int i = 0; i < temp.length(); i++) {
+            stringToInteger << temp.substr(i, 0);
+            stringToInteger >> 
+        }
     }
 }
 
