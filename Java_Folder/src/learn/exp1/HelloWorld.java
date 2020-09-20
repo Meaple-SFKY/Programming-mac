3486f1075b52f326cbd7991dac81aba1cd2da5c2
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-09-20 19:49:08 +0800

    java start

--- Java_Folder/src/learn/exp1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package src.learn.exp1;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
--- Java_Folder/src/learn/preview/FirstSample.java
+++
@@ -1,9 +0,0 @@
-package src.learn.preview;
-
-public class FirstSample
-{
-    public static void main(String[] args)
-    {
-        System.out.println(2.0 - 1.1);
-    }
-}
