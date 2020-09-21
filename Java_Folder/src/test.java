fdaaac593d3534269170ce4a72daeb6482e476e0
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-09-21 20:34:35 +0800

    java

---
+++ Java_Folder/src/test.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.util.*;
+
+class test {
+    public static void main(String[] args) {
+        // System.getProperties().list(System.out);
+        // System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("java.library.path"));
+    }
+}
