d4245dc569df635c0c248e46df4f6b48fbea9d71
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-09-27 11:44:54 +0800

    Java

---
+++ Java_Folder/com/meaple/learn/List.java
@@ -0,0 +1,7 @@
+package com.meaple.learn;
+
+public class List {
+    public List() {
+        System.out.println("com.meaple.learn.List");
+    }
+}
---
+++ Java_Folder/com/meaple/learn/Vector.java
@@ -0,0 +1,7 @@
+package com.meaple.learn;
+
+public class Vector {
+    public Vector() {
+        System.out.println("com.meaple.learn.Vector");
+    }
+}
