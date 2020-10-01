1733d930cc1f92c72a3bb35e15e5b5ca6449822c
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-10-01 12:08:10 +0800

    principle

--- Java_Folder/com/meaple/principle/Analyzer.java
+++ Java_Folder/com/meaple/principle/Analyzer.java
@@ -12,8 +12,8 @@ package com.meaple.principle;
 import java.io.*;
 
 public class Analyzer {
-    public int tempChar;
-    public int tempSlice;
+    public int tempChar = 0;
+    public String tempString = "";
 
     public StringBuffer tempStorString = new StringBuffer();
 
@@ -34,15 +34,13 @@ public class Analyzer {
 
     public char[] delimiters = {
         '.', ',', '(', ')', '[', ']', '{', '}'
-        //46, 44, 41, 40, 91, 93, 123, 125
     };
 
     public char[] arithmeticOperators = {
-        '+', '-', '*', '/', '%'
-        //43, 45, 42, 47
+        '+', '-', '*', '/', '%', '#'
     };
 
-    public String[] ralationalOperators = {
+    public String[] relationalOperators = {
         "<", "<=", "=", ">=", ">", "==", "<>"
     };
 
@@ -80,39 +78,27 @@ public class Analyzer {
     //判断是否是分界符
     public boolean ifIsADelimiter(int slice) {
         int asciiCode = slice;
-        switch (asciiCode) {
-            case 46:
-            case 44:
-            case 41:
-            case 40:
-            case 91:
-            case 93:
-            case 123:
-            case 125: {
+        for (int i = 0; i < delimiters.length; i++) {
+            if (asciiCode == Integer.valueOf(delimiters[i])) {
                 return true;
-            } default: {
-                return false;
             }
         }
+        return false;
     }
 
     //判断是否是算术运算符
     public boolean ifIsAArithmeticOperators(int slice) {
         int asciiCode = slice;
-        switch (asciiCode) {
-            case 43:
-            case 45:
-            case 42:
-            case 47: {
+        for (int i = 0; i < arithmeticOperators.length; i++) {
+            if (asciiCode == Integer.valueOf(arithmeticOperators[i])) {
                 return true;
-            } default: {
-                return false;
             }
         }
+        return false;
     }
 
     //判断是否是关键字
-    public boolean ifIsAKeayWord() {
+    public boolean ifIsAKeyWord() {
         for (int i = 0; i < keyWords.length; i++) {
             if (tempStorString.toString().equals(keyWords[i])) {
                 return true;
@@ -123,7 +109,7 @@ public class Analyzer {
 
     //判断是否是标识符
     public boolean ifIsAIdentifier() {
-        if (tempStorString.charAt(0) >= 48 || tempStorString.charAt(0) <= 57) {
+        if (tempStorString.charAt(0) >= '0' && tempStorString.charAt(0) <= '9') {
             return false;
         } else {
             for (int i = 0; i < tempStorString.length(); i++) {
@@ -140,11 +126,18 @@ public class Analyzer {
 
     //判断是否是常数
     public boolean ifIsASetDigitals() {
+        int temp = 0;
         if (tempStorString.length() != 0) {
             for (int i = 0; i < tempStorString.length(); i++) {
                 if (tempStorString.charAt(i) < 48 || tempStorString.charAt(i) > 57) {
                     return false;
                 }
+                if (tempStorString.charAt(i) == '.') {
+                    temp++;
+                }
+            }
+            if (temp > 1) {
+                return false;
             }
             return true;
         } else {
@@ -152,25 +145,65 @@ public class Analyzer {
         }
     }
 
-    //连接经过处理的字符串和刚处理的字符
-    public void addCharToString(char slice) {
-        tempStorString.append(slice);
+    //判断是否是单个关系运算符
+    public boolean ifIsARelaOperator(int slice) {
+        if ((char)slice == '=' || (char)slice == '<' || (char)slice == '>') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    //刷新临时存储字符串
-    public void flushString() {
-        if (ifIsAKeayWord() == true) {
-            System.out.println(tempStorString + "\t" + "(" + 1 + ", " + tempStorString
-                + ")\t" + "关键字\t");
-        } else if (ifIsASetDigitals() == true) {
-            System.out.println(tempStorString + "\t" + "(" + 3 + ", " + tempStorString
-                + ")\t" + "常数\t");
-        } else if (ifIsAIdentifier() == true) {
-            System.out.println(tempStorString + "\t" + "(" + 2 + ", " + tempStorString
-                + ")\t" + "标识符\t");
-        } else {
-            System.out.println(tempStorString + "\t" + "ERROR\t" + "ERROR\t");
+    //判断是否是关系运算符
+    public boolean ifIsRelaOperator() {
+        for (int i = 0; i < relationalOperators.length; i++) {
+            if (tempStorString.equals(relationalOperators[i]) == true) {
+                return true;
+            }
         }
+        return false;
+    }
+
+    //连接经过处理的字符串和刚处理的字符
+    public void addCharToString(int slice) {
+        tempStorString.append((char)slice);
+    }
+
+    //输出关键字、标识符、常数、关系运算符有关信息
+    public void printString() {
+        if (tempStorString.length() != 0) {
+            if (ifIsAKeyWord() == true) {
+                System.out.println(tempStorString + "\t(" + 1 + ", " + tempStorString
+                                + ")\t" + "关键字\t");
+            } else if (ifIsASetDigitals() == true) {
+                System.out.println(tempStorString + "\t(" + 3 + ", " + tempStorString
+                                + ")\t" + "常数\t");
+            } else if (ifIsAIdentifier() == true) {
+                System.out.println(tempStorString + "\t(" + 2 + ", " + tempStorString
+                                + ")\t" + "标识符\t");
+            } else if (ifIsRelaOperator() == true) {
+                System.out.println(tempStorString + "\t(" + 5 + ", " + tempStorString
+                                + ")\t" + "关系运算符\t");
+            } else {
+                System.out.println(tempStorString + "\tERROR\tERROR\t");
+            }
+        }
+    }
+
+    //输出分界符有关信息
+    public void printDelimiter(int slice) {
+        System.out.println((char)slice + "\t(" + 6 + ", " + (char)slice
+                            + ")\t" + "分界符\t");
+    }
+
+    //输出运算符有关信息
+    public void printOperator(int slice) {
+        System.out.println((char)slice + "\t(" + 4 + ", " + (char)slice
+                            + ")\t" + "运算符\t");
+    }
+
+    //刷新临时字符串
+    public void flushTempString() {
         tempStorString.delete(0, tempStorString.length());
     }
 
@@ -179,15 +212,46 @@ public class Analyzer {
         try {
             bufferedReader = new BufferedReader(new FileReader(fileName));
             while ((tempChar = bufferedReader.read()) != -1) {
-                if (ifIsASpace(tempChar) == false) {
-                    if(ifIsALetter(tempChar) == true || ifIsADigital(tempChar) == true) {
-                        addCharToString((char)tempChar);
+                if (ifIsASpace(tempChar) == false && (char)tempChar != '\n') {
+                    if (ifIsALetter(tempChar) == true || ifIsADigital(tempChar) == true ||
+                        (char)tempChar == '_') {
+                        addCharToString(tempChar);
+                    } else if (ifIsADelimiter(tempChar)) {
+                        if ((char)tempChar == '.') {
+                            if (ifIsASetDigitals() == true) {
+                                addCharToString(tempChar);
+                            } else {
+                                printString();
+                                flushTempString();
+                                printDelimiter(tempChar);
+                            }
+                        } else {
+                            printString();
+                            flushTempString();
+                            printDelimiter(tempChar);
+                        }
                     } else if (ifIsAArithmeticOperators(tempChar) == true) {
-                        System.out.println((char)tempChar + "\t" + "(" + 4 + ", " + (char)tempChar
-                        + ")\t" + "运算符\t");
+                        printString();
+                        flushTempString();
+                        printOperator(tempChar);
+                    } else if (ifIsARelaOperator(tempChar) == true) {
+                        if (tempStorString.length() > 0) {
+                            if (ifIsARelaOperator(tempStorString.charAt(0)) == false) {
+                                printString();
+                                flushTempString();
+                                addCharToString(tempChar);
+                            } else {
+                                addCharToString(tempChar);
+                                printString();
+                                flushTempString();
+                            }
+                        } else {
+                            addCharToString(tempChar);
+                        }
                     }
-                    //
-                    //处理关系运算符
+                } else {
+                    printString();
+                    flushTempString();
                 }
             }
         } catch (FileNotFoundException exceptionFile) {
@@ -195,5 +259,11 @@ public class Analyzer {
         } catch (IOException exceptionIO) {
             exceptionIO.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        Analyzer analyzer = new Analyzer();
+        String fileName = new String("/Users/sfky/Documents/Programming/Java_Folder/com/meaple/principle/example.c");
+        analyzer.analysis(fileName);
     }
 }
