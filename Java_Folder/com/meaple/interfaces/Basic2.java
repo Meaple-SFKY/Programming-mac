9941827f38a0647f4465e02f6f289e27d06fbe11
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-10-08 21:20:07 +0800

    principle

---
+++ Java_Folder/com/meaple/interfaces/Basic2.java
@@ -0,0 +1,13 @@
+package com.meaple.interfaces;
+
+abstract class Basic2 extends Basic {
+    int f() {
+        return 111;
+    }
+
+    @Override
+    void unimplemented() {
+        // TODO Auto-generated method stub
+        
+    }
+}
