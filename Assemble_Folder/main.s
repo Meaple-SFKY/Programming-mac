236959ebb3569eab9282f6f6dbb484ae932d76d7
Meaple-SFKY <xuwenxing0923@outlook.com>
2020-12-03 10:16:20 +0800

    assamble

--- Assemble_Folder/main.s
+++ Assemble_Folder/main.s
@@ -10,33 +10,11 @@ _main:                                  ## @main
 	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register %rbp
-	subq	$16, %rsp
-	leaq	-8(%rbp), %rdx
-	movl	$2, %edi
-	movl	$3, %esi
-	callq	_multstore
-	movq	-8(%rbp), %rsi
 	leaq	L_.str(%rip), %rdi
+	movl	$2, %esi
 	xorl	%eax, %eax
 	callq	_printf
 	xorl	%eax, %eax
-	addq	$16, %rsp
-	popq	%rbp
-	retq
-	.cfi_endproc
-                                        ## -- End function
-	.globl	_mult2                  ## -- Begin function mult2
-	.p2align	4, 0x90
-_mult2:                                 ## @mult2
-	.cfi_startproc
-## %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	movq	%rdi, %rax
-	imulq	%rsi, %rax
 	popq	%rbp
 	retq
 	.cfi_endproc
--- Assemble_Folder/mstore.s
+++ Assemble_Folder/mstore.s
@@ -10,14 +10,18 @@ _multstore:                             ## @multstore
 	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register %rbp
-	pushq	%rbx
-	pushq	%rax
-	.cfi_offset %rbx, -24
-	movq	%rdx, %rbx
+	subq	$32, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	%rdx, -24(%rbp)
+	movq	-8(%rbp), %rdi
+	movq	-16(%rbp), %rsi
 	callq	_mult2
-	movq	%rax, (%rbx)
-	addq	$8, %rsp
-	popq	%rbx
+	movq	%rax, -32(%rbp)
+	movq	-32(%rbp), %rax
+	movq	-24(%rbp), %rcx
+	movq	%rax, (%rcx)
+	addq	$32, %rsp
 	popq	%rbp
 	retq
 	.cfi_endproc
